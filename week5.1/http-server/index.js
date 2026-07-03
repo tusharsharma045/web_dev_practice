f4edcef76101e2b6aeefea2ad72acf9e5a7b5dd1
Tushar Sharma <tusharsharma02007@gmail.com>
2026-07-03 22:44:26 +0530

    feat : https server

---
+++ week5.1/http-server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+let requestcount = 0;
+
+function requestincreaser(req,res,next){
+    requestcount += 1;
+    console.log(`Request count: ${requestcount}`);
+}
+
+function realsummhandler(req,res){
+    const a = parseInt(req.query.firstarg)
+    const b = parseInt(req.query.secondarg)
+    res.json({
+        answer : a+b
+    });
+};
+// app.get("/sum",function (req, res) {
+// requestincreaser();
+//     const a = req.query.a
+//     const b = req.query.b
+
+//     res.json({
+//         answer : a+b
+
+//     });
+    
+// });
+app.get("/sum",requestincreaser,realsummhandler);
+// 
+app.get("/sum",function (req, res) {
+    // requestincreaser(); // use the function on both places in .get and out of .get
+    const a = parseInt(req.query.firstarg)
+    const b = parseInt(req.query.secondarg)
+
+    res.json({
+        answer : a+b
+
+    });
+});
+
+
+// app.get("multiply",function (req,res) {
+    
+// });
+
+
+app.listen(3000);
