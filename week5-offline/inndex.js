d2a9443cd3e6801e5a40cda32f2ce6a3de3e9eaa
Tushar Sharma <tusharsharma02007@gmail.com>
2026-07-04 20:56:11 +0530

    feat : map,filter

---
+++ week5-offline/inndex.js
@@ -0,0 +1,24 @@
+// map filter,arrow
+
+// const input = [1, 2, 3, 4, 5];
+
+// const newarr = [];
+
+// for (let i = 0; i < input.length; i++) {
+//   newarr.push(input[i] * 2);
+// };
+
+// other solution
+
+const input = [1, 2, 3, 4, 5];
+
+const newarr = input.map((item) => item * 2);
+console.log(newarr);
+
+
+// filter
+
+const input2 = [1, 2, 3, 4, 5];
+
+const newarr2 = input2.filter((item) => item > 3);
+console.log(newarr2);
