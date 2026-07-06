007370f8e4a380d5759b28dcd213e11391cb34f2
Tushar Sharma <tusharsharma02007@gmail.com>
2026-07-06 18:25:28 +0530

    feat : jsonwebtokens

--- week6.1/index.js
+++ week6.1/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "harkiratsinghbassi"; 
 const app = express();
 
 app.use(express.json());
@@ -15,6 +17,8 @@ function generateToken() {
     }
     return token;
 }
+
+
 
 app.post('/signup', (req, res) => {
 
@@ -40,16 +44,35 @@ app.post('/signin', (req, res) => {
 
     const userfound = users.find(function(u){
 
-        if (u.username == username && u.password == password){
+        if (u.username === username && u.password === password){
             return true}
 
         else{
             return false
         }
 
+
+        // without using jwt tokens;
+        // if (userfound){
+        //     const token = generateToken();
+        //     userfound.token= token
+
+        //     res.json({
+        //         token : token
+        //     })
+        // }else{
+        //     res.status(403).send({
+        //         message: "invalid username and password" 
+        //     })
+        // }
+        
+
+        // by using jwt tokens; it is stateless token 
         if (userfound){
-            const token = generateToken();
-            userfound.token= token
+            const token = jwt.sign({
+                username: username
+            }, JWT_SECRET);
+           
 
             res.json({
                 token : token
@@ -59,10 +82,56 @@ app.post('/signin', (req, res) => {
                 message: "invalid username and password" 
             })
         }
-        
+
     })
 });
 
+
+// app.post('/signin', (req, res) => {
+//     const { username, password } = req.body;
+
+//     const userfound = users.find(
+//         u => u.username === username && u.password === password
+//     );
+
+//     if (!userfound) {
+//         return res.status(403).json({
+//             message: "Invalid username and password"
+//         });
+//     }
+
+//     const token = generateToken();
+//     userfound.token = token;
+
+//     res.json({
+//         token
+//     });
+// });
+
+app.get("/me", (req, res) => {
+    const token = req.headers.authorization;
+    const decodedinformation = jwt.verify(token, JWT_SECRET);
+    const unauthdecodeinfo = jwt.decode(token, JWT_SECRET);
+    const username = decodedinformation.username;   
+
+    let username = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token === token) {
+            founduser = users[i];
+            
+        }
+    }
+    if (founduser) {
+        res.send({
+            username: founduser.username,
+            password: founduser.password
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+})
 
 
 app.listen(3000);
