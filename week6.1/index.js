2460172168164678e220265398513a1d1a6cc232
Tushar Sharma <tusharsharma02007@gmail.com>
2026-07-06 15:48:07 +0530

    feat : signup-signin server

---
+++ week6.1/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post('/signup', (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: 'User created successfully',
+        users: users
+    });
+});
+
+
+app.post('/signin', (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userfound = users.find(function(u){
+
+        if (u.username == username && u.password == password){
+            return true}
+
+        else{
+            return false
+        }
+
+        if (userfound){
+            const token = generateToken();
+            userfound.token= token
+
+            res.json({
+                token : token
+            })
+        }else{
+            res.status(403).send({
+                message: "invalid username and password" 
+            })
+        }
+        
+    })
+});
+
+
+
+app.listen(3000);
