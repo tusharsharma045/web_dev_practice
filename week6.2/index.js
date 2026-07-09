8a713a26e90946c99ec5191fe211c8d2d72cf7d0
tusharsharma045 <tusharsharma02007@gmail.com>
2026-07-09 20:13:57 +0530

    feat : signin-signup

--- week6.2/index.js
+++ week6.2/index.js
@@ -1,26 +1,84 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const app = express();
-const jwt   = require('jsonwebtoken');
 
-const JWT_SECRET = 'HARKIRATSINGHBASSI'; // Replace
-app.use(express.json());    
+app.use(express.json());
 
-app.post('/signup', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+const JWT_SECRET = "HARKIRATSINGHBASSI";
+
+const users = [];
+
+app.post("/signup", (req, res) => {
+    const { username, password } = req.body;
+
+    users.push({
+        username,
+        password
+    });
 
     res.json({
-        message: 'User signed up successfully',
-        
+        message: "User signed up successfully"
     });
 });
 
-app.post('/signin', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/signin", (req, res) => {
+    const { username, password } = req.body;
 
-    let foundUser = [i];
+    let foundUser = null;
 
+    for (let i = 0; i < users.length; i++) {
+        if (
+            users[i].username === username &&
+            users[i].password === password
+        ) {
+            foundUser = users[i];
+            break;
+        }
+    }
+
+    if (!foundUser) {
+        return res.json({
+            message: "Invalid credentials"
+        });
+    }
+
+    const token = jwt.sign({ username }, JWT_SECRET);
+
+    res.json({
+        token
+    });
 });
 
-app.listen(3000);
+app.get("/me", (req, res) => {
+    const token = req.headers.token;
+
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
+
+        let foundUser = null;
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username === decodedData.username) {
+                foundUser = users[i];
+                break;
+            }
+        }
+
+        if (!foundUser) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.json(foundUser);
+    } catch (err) {
+        res.status(401).json({
+            message: "Invalid token"
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
